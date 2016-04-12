0044eb6aea257187e0008939eb2da4b6371b3dd5
pdean1 <pdean1@my.westga.edu>
2016-04-12 19:42:30 -0400

    Initial commit.

---
+++ app/src/main/java/edu/westga/cs6242/budgetingapplication/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package edu.westga.cs6242.budgetingapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
