60dd79786a289e690f03228858e31fb9b15429d7
pdean1 <pdean1@my.westga.edu>
2016-04-18 16:44:38 -0400

    Added create table statements to each of the inner classes in the database utility file. These values can be used by the handler to create the database.

--- app/src/main/java/edu/westga/cs6242/budgetingapplication/util/database/BudgetDatabase.java
+++ app/src/main/java/edu/westga/cs6242/budgetingapplication/util/database/BudgetDatabase.java
@@ -9,6 +9,9 @@ package edu.westga.cs6242.budgetingapplication.util.database;
  */
 public class BudgetDatabase {
 
+    public static final String DATABASE_NAME    = "Budget.db";
+    public static final int    DATABASE_VERSION = 1;
+
     /**
      * Public inner class Users defines strings for the user table
      */
@@ -18,16 +21,27 @@ public class BudgetDatabase {
         public static final String C2_USER_NAME = "user_name";
         public static final String C3_PASSWORD  = "password";
         public static final String C4_EMAIL     = "email";
+        public static final String CREATE_USERS_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " ( "
+                + C1_PK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + C2_USER_NAME + " TEXT NOT NULL, "
+                + C3_PASSWORD + " TEXT NOT NULL, "
+                + C4_EMAIL + " TEXT NOT NULL); ";
     }
 
     /**
      * Public inner class AccountType defines strings for the AccountType table
      */
-    public class AccountType {
-        public static final String TABLE_NAME     = "AccountType";
+    public class AccountTypes {
+        public static final String TABLE_NAME     = "AccountTypes";
         public static final String C1_PK_ID       = "id";
         public static final String C2_TITLE       = "title";
         public static final String C3_DESCRIPTION = "description";
+        public static final String CREATE_ACOUNT_TYPES_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " ( "
+                + C1_PK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + C2_TITLE  + " TEXT NOT NULL, "
+                + C3_DESCRIPTION + "TEXT);";
     }
 
     /**
@@ -41,6 +55,18 @@ public class BudgetDatabase {
         public static final String C4_DATE_UPDATED        = "date_updated";
         public static final String C5_FK1_USER_ID         = "user_id";
         public static final String C6_FK2_ACCOUNT_TYPE_ID = "account_type_id";
+        public static final String CREATE_ACCOUNTS_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " ( "
+                        + C1_PK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        + C2_DESCRIPTION + " TEXT NOT NULL "
+                        + C3_DATE_CREATED + " NUMERIC NOT NULL, "
+                        + C4_DATE_UPDATED + " NUMERIC, "
+                        + C5_FK1_USER_ID + " INTEGER NOT NULL ,"
+                        + C6_FK2_ACCOUNT_TYPE_ID + " INTEGER NOT NULL "
+                + "FOREIGN KEY (" + C5_FK1_USER_ID + ") REFERENCES "
+                        + Users.TABLE_NAME+" ( " + Users.C1_PK_ID + " ) "
+                + "FOREIGN KEY (" + C6_FK2_ACCOUNT_TYPE_ID + ") REFERENCES "
+                        + AccountTypes.TABLE_NAME+" ( " + AccountTypes.C1_PK_ID + " ) ";
     }
 
     /**
@@ -54,22 +80,16 @@ public class BudgetDatabase {
         public static final String C4_DATE_CREATED = "date_created";
         public static final String C5_DATE_UPDATED = "date_updated";
         public static final String C6_FK1_USER_ID  = "user_id";
-    }
-
-    /**
-     * Public inner class Transfers defines strings for the Transfers table
-     */
-    public class Transfers {
-        public static final String TABLE_NAME             = "Transfers";
-        public static final String C1_PK_ID               = "id";
-        public static final String C2_TITLE               = "title";
-        public static final String C3_AMOUNT              = "amount";
-        public static final String C4_DATE_OF_TRANSFER    = "date_of_transfer";
-        public static final String C5_IS_RECURRING        = "is_recurring";
-        public static final String C6_FK1_ACCOUNT_FROM_ID = "account_from_id";
-        public static final String C7_FK2_ACCOUNT_TO_ID   = "account_to_id";
-        public static final String C8_FK3_BUDGET_ID       = "budget_id";
-        public static final String C9_FK4_USER_ID         = "user_id";
+        public static final String CREATE_MONTHLY_BUDGET_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " ( "
+                        + C1_PK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        + C2_TITLE + " TEXT NOT NULL "
+                        + C3_DESCRIPTION + " TEXT, "
+                        + C4_DATE_CREATED + " NUMERIC NOT NULL, "
+                        + C5_DATE_UPDATED + " NUMERIC,"
+                        + C6_FK1_USER_ID + " INTEGER NOT NULL "
+                        + "FOREIGN KEY (" + C6_FK1_USER_ID + ") REFERENCES "
+                        + Users.TABLE_NAME+" ( " + Users.C1_PK_ID + " ) ";
     }
 
     /**
@@ -85,6 +105,18 @@ public class BudgetDatabase {
         public static final String C6_IS_PAID             = "is_paid";
         public static final String C7_IS_RECURRING        = "is_recurring";
         public static final String C8_FK1_BUDGET_ID       = "budget_id";
+        public static final String CREATE_BILLS_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " ( "
+                        + C1_PK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        + C2_TITLE + " TEXT NOT NULL "
+                        + C3_AMOUNT + " NUMERIC NOT NULL, "
+                        + C4_DATE_DUE + " NUMERIC NOT NULL, "
+                        + C5_DATE_PAID + " NUMERIC,"
+                        + C6_IS_PAID + " INTEGER NOT NULL,"
+                        + C7_IS_RECURRING + " INTEGER NOT NULL,"
+                        + C8_FK1_BUDGET_ID + " INTEGER NOT NULL,"
+                        + "FOREIGN KEY (" + C8_FK1_BUDGET_ID + ") REFERENCES "
+                        + MonthlyBudget.TABLE_NAME + " ( " + MonthlyBudget.C1_PK_ID + " ) ";
     }
 
     /**
@@ -98,5 +130,15 @@ public class BudgetDatabase {
         public static final String C4_DATE_EARNED         = "date_earned";
         public static final String C5_IS_RECURRING        = "is_recurring";
         public static final String C6_FK1_BUDGET_ID       = "budget_id";
+        public static final String CREATE_EARNINGS_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " ( "
+                        + C1_PK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        + C2_TITLE + " TEXT NOT NULL "
+                        + C3_AMOUNT + " NUMERIC NOT NULL, "
+                        + C4_DATE_EARNED + " NUMERIC NOT NULL, "
+                        + C5_IS_RECURRING + " INTEGER NOT NULL,"
+                        + C6_FK1_BUDGET_ID + " INTEGER NOT NULL,"
+                        + "FOREIGN KEY (" + C6_FK1_BUDGET_ID + ") REFERENCES "
+                        + MonthlyBudget.TABLE_NAME + " ( " + MonthlyBudget.C1_PK_ID + " ) ";
     }
 }
