528fc0586996402e3be57b73474bbe49b9a98e84
pdean1 <pdean1@my.westga.edu>
2016-04-19 22:41:32 -0400

    Added insert statements for the Budget databases tables

--- app/src/main/java/edu/westga/cs6242/budgetingapplication/util/database/BudgetDatabase.java
+++ app/src/main/java/edu/westga/cs6242/budgetingapplication/util/database/BudgetDatabase.java
@@ -13,6 +13,7 @@ public class BudgetDatabase {
 
     public static final String DATABASE_NAME    = "Budget.db";
     public static final int    DATABASE_VERSION = 1;
+    public static final String INSERT_INTO = "INSERT INTO ";
 
     /**
      * Uses the passed database to create tables in it
@@ -20,7 +21,7 @@ public class BudgetDatabase {
      */
     public static void createDatabase(SQLiteDatabase db) {
         db.execSQL(Users.CREATE_USERS_TABLE);
-        db.execSQL(AccountTypes.CREATE_ACOUNT_TYPES_TABLE);
+        db.execSQL(AccountTypes.CREATE_ACCOUNT_TYPES_TABLE);
         db.execSQL(Accounts.CREATE_ACCOUNTS_TABLE);
         db.execSQL(MonthlyBudget.CREATE_MONTHLY_BUDGET_TABLE);
         db.execSQL(Bills.CREATE_BILLS_TABLE);
@@ -35,13 +36,14 @@ public class BudgetDatabase {
         public static final String C1_PK_ID     = "id";
         public static final String C2_USER_NAME = "user_name";
         public static final String C3_PASSWORD  = "password";
-        public static final String C4_EMAIL     = "email";
         public static final String CREATE_USERS_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " ( "
                 + C1_PK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + C2_USER_NAME + " TEXT NOT NULL, "
-                + C3_PASSWORD + " TEXT NOT NULL, "
-                + C4_EMAIL + " TEXT NOT NULL); ";
+                + C3_PASSWORD + " TEXT NOT NULL); ";
+        public static final String INSERT_USERS_QUERY = INSERT_INTO +
+                TABLE_NAME + "(" + C2_USER_NAME + ", " + C3_PASSWORD + ")" +
+                " VALUES (@user_name, @password)";
     }
 
     /**
@@ -52,11 +54,16 @@ public class BudgetDatabase {
         public static final String C1_PK_ID       = "id";
         public static final String C2_TITLE       = "title";
         public static final String C3_DESCRIPTION = "description";
-        public static final String CREATE_ACOUNT_TYPES_TABLE =
+        public static final String CREATE_ACCOUNT_TYPES_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " ( "
                 + C1_PK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + C2_TITLE  + " TEXT NOT NULL, "
                 + C3_DESCRIPTION + "TEXT);";
+        public static final String INSERT_ACCOUNT_TYPES_QUERY =
+                INSERT_INTO + TABLE_NAME + "(" +
+                        C2_TITLE + ", " +
+                        C3_DESCRIPTION +
+                        ") VALUES (@title, @description)";
     }
 
     /**
@@ -82,7 +89,11 @@ public class BudgetDatabase {
                         + Users.TABLE_NAME+" ( " + Users.C1_PK_ID + " ) "
                 + "FOREIGN KEY (" + C6_FK2_ACCOUNT_TYPE_ID + ") REFERENCES "
                         + AccountTypes.TABLE_NAME+" ( " + AccountTypes.C1_PK_ID + " ) ";
-        public static final String DROP_ACCOUNTS_TABLE = "";
+        public static final String INSERT_ACCOUNTS_QUERY = INSERT_INTO + TABLE_NAME +
+                "("+ C2_DESCRIPTION + ", " +
+                C3_DATE_CREATED + ", " + C4_DATE_UPDATED + ", " +
+                C5_FK1_USER_ID + ", " + C6_FK2_ACCOUNT_TYPE_ID +
+                ") VALUES (@description, @date_created, @date_updated, @user_id, @account_type);";
     }
 
     /**
@@ -106,6 +117,11 @@ public class BudgetDatabase {
                         + C6_FK1_USER_ID + " INTEGER NOT NULL "
                         + "FOREIGN KEY (" + C6_FK1_USER_ID + ") REFERENCES "
                         + Users.TABLE_NAME+" ( " + Users.C1_PK_ID + " ) ";
+        public static final String INSERT_MONTHLY_BUDGET_QUERY = INSERT_INTO + TABLE_NAME +
+                "("+ C2_TITLE + ", " +
+                C3_DESCRIPTION + ", " + C4_DATE_CREATED + ", " +
+                C5_DATE_UPDATED + ", " + C6_FK1_USER_ID +
+                ") VALUES (@title, @description, @date_created, @date_updated, @user_id);";
     }
 
     /**
@@ -133,6 +149,13 @@ public class BudgetDatabase {
                         + C8_FK1_BUDGET_ID + " INTEGER NOT NULL,"
                         + "FOREIGN KEY (" + C8_FK1_BUDGET_ID + ") REFERENCES "
                         + MonthlyBudget.TABLE_NAME + " ( " + MonthlyBudget.C1_PK_ID + " ) ";
+        public static final String INSERT_BILL_QUERY = INSERT_INTO + TABLE_NAME +
+                "("+ C2_TITLE + ", " +
+                C3_AMOUNT + ", " + C4_DATE_DUE + ", " +
+                C5_DATE_PAID + ", " + C6_IS_PAID + ", " +
+                C7_IS_RECURRING + ", " + C8_FK1_BUDGET_ID +
+                ") VALUES (@title, @amount, @date_due, @date_paid, @is_paid," +
+                " @is_recurring, @budget_id);";
     }
 
     /**
@@ -156,5 +179,10 @@ public class BudgetDatabase {
                         + C6_FK1_BUDGET_ID + " INTEGER NOT NULL,"
                         + "FOREIGN KEY (" + C6_FK1_BUDGET_ID + ") REFERENCES "
                         + MonthlyBudget.TABLE_NAME + " ( " + MonthlyBudget.C1_PK_ID + " ) ";
+        public static final String INSERT_EARNINGS_QUERY = INSERT_INTO + TABLE_NAME +
+                "("+ C2_TITLE + ", " +
+                C3_AMOUNT + ", " + C4_DATE_EARNED + ", " +
+                C5_IS_RECURRING + ", " + C6_FK1_BUDGET_ID +
+                ") VALUES (@title, @amount, @date_earned, @is_recurring, @budget_id);";
     }
 }
