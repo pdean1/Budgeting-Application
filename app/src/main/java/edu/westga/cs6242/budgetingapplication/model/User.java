8365dfcb33f0cabdbabfe5ad49995f4015cae2d4
pdean1 <pdean1@my.westga.edu>
2016-04-22 10:47:04 -0400

    Updated the user class

--- app/src/main/java/edu/westga/cs6242/budgetingapplication/model/User.java
+++ app/src/main/java/edu/westga/cs6242/budgetingapplication/model/User.java
@@ -1,9 +1,53 @@
 package edu.westga.cs6242.budgetingapplication.model;
 
-import edu.westga.cs6242.budgetingapplication.model.base.BUser;
-
 /**
  * Created by Patrick on 4/19/2016.
  */
-public class User extends BUser {
+public class User {
+    private int id;
+    private String userName;
+    private String password;
+
+    /**
+     * Default User Constructor
+     */
+    public User() {
+        this(0, "", "");
+    }
+
+    /**
+     * Creates a User with custom values
+     * @param id       User's ID or 0
+     * @param userName User's User Name
+     * @param password User's Password
+     */
+    public User(int id, String userName, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
