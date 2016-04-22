16942e4388b5cbc723b7b6e8e69d9bf0b1b26696
pdean1 <pdean1@my.westga.edu>
2016-04-22 10:53:34 -0400

    Updated the AccountType Class

--- app/src/main/java/edu/westga/cs6242/budgetingapplication/model/AccountType.java
+++ app/src/main/java/edu/westga/cs6242/budgetingapplication/model/AccountType.java
@@ -1,7 +1,46 @@
 package edu.westga.cs6242.budgetingapplication.model;
 
 /**
- * Created by Patrick on 4/19/2016.
+ * Represents an Account Type
+ * @author Patrick Dean
+ * @version 1
  */
 public class AccountType {
+    private int id;
+    private String title;
+    private String description;
+
+    public AccountType() {
+        this(0, "", "");
+    }
+
+    public AccountType(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
