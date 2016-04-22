66972dc841f2e3f08dfb0b3602956be2053104d6
pdean1 <pdean1@my.westga.edu>
2016-04-22 11:05:14 -0400

    Updated the Account Class

--- app/src/main/java/edu/westga/cs6242/budgetingapplication/model/Account.java
+++ app/src/main/java/edu/westga/cs6242/budgetingapplication/model/Account.java
@@ -1,7 +1,80 @@
 package edu.westga.cs6242.budgetingapplication.model;
 
+import java.util.Date;
+
 /**
- * Created by Patrick on 4/19/2016.
+ * Represents an account
+ * @author Patrick Dean
+ * @version 1
  */
 public class Account {
+    private int id;
+    private String description;
+    private Date dateCreated;
+    private Date dateUpdated;
+    private int userId;
+    private int accountTypeId;
+
+    public Account() {
+        this(0, "", new Date(), new Date(), 0, 0);
+    }
+
+    public Account(int id, String description, Date dateCreated,
+                   Date dateUpdated, int userId, int accountTypeId)
+    {
+        this.id = id;
+        this.description = description;
+        this.dateCreated = dateCreated;
+        this.dateUpdated = dateUpdated;
+        this.userId = userId;
+        this.accountTypeId = accountTypeId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(Date dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getAccountTypeId() {
+        return accountTypeId;
+    }
+
+    public void setAccountTypeId(int accountTypeId) {
+        this.accountTypeId = accountTypeId;
+    }
 }
