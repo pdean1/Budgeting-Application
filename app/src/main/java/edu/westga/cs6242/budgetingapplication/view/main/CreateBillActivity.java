02cc109ce05845116450447e09dbf3ca223a3d01
pdean1 <pdean1@my.westga.edu>
2016-04-24 02:24:37 -0400

    user can now add a bill

--- app/src/main/java/edu/westga/cs6242/budgetingapplication/view/main/CreateBillActivity.java
+++ app/src/main/java/edu/westga/cs6242/budgetingapplication/view/main/CreateBillActivity.java
@@ -2,17 +2,59 @@ package edu.westga.cs6242.budgetingapplication.view.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Date;
 
 import edu.westga.cs6242.budgetingapplication.R;
+import edu.westga.cs6242.budgetingapplication.dal.BudgetDatabaseHandler;
+import edu.westga.cs6242.budgetingapplication.model.Bill;
+import edu.westga.cs6242.budgetingapplication.model.Session;
 
 public class CreateBillActivity extends AppCompatActivity {
 
+    private EditText etTitle, etAmount, etDateDue;
+    private CheckBox cbIsRecurring, cbIsPaid;
+
+    private BudgetDatabaseHandler dbh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_bill);
+        this.dbh = new BudgetDatabaseHandler(getApplicationContext(), null);
+        this.etTitle = (EditText) findViewById(R.id.etBillTitle);
+        this.etAmount = (EditText) findViewById(R.id.etBillAmount);
+        this.etDateDue = (EditText) findViewById(R.id.etDateDue);
+        this.cbIsPaid = (CheckBox) findViewById(R.id.cbIsPaid);
+        this.cbIsRecurring = (CheckBox) findViewById(R.id.cbIsRecurring);
     }
 
+
+    public void btnAddBill_Click(View v) {
+        Bill bill = new Bill();
+        bill.setTitle(this.etTitle.getText().toString());
+        bill.setAmount(Double.parseDouble(this.etAmount.getText().toString()));
+        bill.setDateDue(new Date());
+        bill.setDatePaid(new Date());
+        bill.setIsRecurring(this.cbIsRecurring.isChecked());
+        bill.setIsPaid(this.cbIsPaid.isChecked());
+        bill.setBudgetId(Session.getMonthlyBudget1().getId());
+        long result = this.dbh.addBill(bill);
+        if (result == -1) {
+            ToastMessage("Unable to add Bill");
+            return;
+        }
+        ToastMessage("Bill Added");
+    }
+
+    private void ToastMessage(String text) {
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+        toast.show();
+    }
 
 }
