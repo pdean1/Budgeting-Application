a175b7f4d353e577284629d539f4e2dd6f0d6e5f
pdean1 <pdean1@my.westga.edu>
2016-04-23 20:03:12 -0400

    updating application finctionality commit

--- app/src/main/java/edu/westga/cs6242/budgetingapplication/view/main/ManageBudgetActivity.java
+++ app/src/main/java/edu/westga/cs6242/budgetingapplication/view/main/ManageBudgetActivity.java
@@ -2,6 +2,7 @@ package edu.westga.cs6242.budgetingapplication.view.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
 
 import edu.westga.cs6242.budgetingapplication.R;
 import edu.westga.cs6242.budgetingapplication.model.MonthlyBudget;
@@ -17,5 +18,17 @@ public class ManageBudgetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_budget);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        assert tabHost != null;
+        tabHost.setup();
+        TabHost.TabSpec spec = tabHost.newTabSpec("Bills Tab");
+        spec.setContent(R.id.bills_tab);
+        spec.setIndicator("Bills Tab");
+        tabHost.addTab(spec);
+        spec = tabHost.newTabSpec("Earnings Tab");
+        spec.setContent(R.id.earnings_tab);
+        spec.setIndicator("Earnings Tab");
+        tabHost.addTab(spec);
     }
 }
